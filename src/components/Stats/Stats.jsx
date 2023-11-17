3a191da0956beeca1bc0965377aa5dcf196fae80
ssimola <sami.simola@edu.sasky.fi>
2023-11-17 12:22:46 +0200

    Ulkoasu viilausta

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -12,31 +12,33 @@ function Stats(props) {
       length: item.length
     }));
 
-  return (
-    <div className={styles.stats}>
-      <h2>Stats</h2>
-      <h2>Juoksutilastot</h2>
-      <div className={styles.chartContainer}>
-        <ResponsiveContainer height={350}>
-          <BarChart data={bardata}>
+    return (
+      <div className={styles.stats}>
+        <h2>Stats</h2>
+        <h2>Juoksutilastot</h2>
+        <div className={styles.chartContainer}>
+          <ResponsiveContainer height={350}>
+          <BarChart data={bardata} categoryGap={10}>
             <Bar dataKey='length' fill='#FF0000' barSize={30} />
-            <XAxis
-              padding={{ left: 15, right: 15 }}
-              type='number'
-              dataKey='date'
-              domain={['dataMin', 'dataMax']}
-              tickFormatter={value => new Date(value).toLocaleDateString(locale)}
-            />
-            <YAxis />
-            <Tooltip
-              labelFormatter={value => new Date(value).toLocaleDateString(locale)}
-              formatter={value => [numberFormat.format(value), "Juostu"]}
-            />
-          </BarChart>
-        </ResponsiveContainer>
+              <XAxis
+                padding={{ left: 15, right: 15 }}
+                type='number'
+                dataKey='date'
+                domain={['dataMin', 'dataMax']}
+                tickFormatter={value => new Date(value).toLocaleDateString(locale)}
+              />
+              <YAxis />
+              <Tooltip
+                labelFormatter={value => new Date(value).toLocaleDateString(locale)}
+                formatter={value => [numberFormat.format(value), "Juostu"]}
+                contentStyle={{ background: '#fff', border: '1px solid #ccc' }}
+                cursor={{ fill: 'transparent' }}
+              />
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 
 export default Stats;
