6da62df136ab35d02443404870086a8f92b9153f
ssimola <sami.simola@edu.sasky.fi>
2023-11-17 11:47:44 +0200

    muuttaa kaavion takaisin lineChartiksi

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -1,11 +1,11 @@
 import styles from './Stats.module.scss';
-import { Bar, BarChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import { LineChart, Line, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 function Stats(props) {
   const locale = "fi-FI";
   const numberFormat = new Intl.NumberFormat(locale, { style: 'unit', unit: 'kilometer', unitDisplay: 'long' });
 
-  const bardata = props.data
+  const linedata = props.data
     .filter(item => item.type === "Juoksu")
     .map(item => ({
       date: new Date(item.date).getTime(),
@@ -14,27 +14,24 @@ function Stats(props) {
 
   return (
     <div className={styles.stats}>
-      <h2>Stats</h2>
-      <h2>Juoksutilastot</h2>
-      <div className={styles.chartContainer}>
-        <ResponsiveContainer height={350}>
-          <BarChart data={bardata}>
-            <Bar dataKey='length' fill='#FF0000' barSize={30} />
-            <XAxis
-              padding={{ left: 15, right: 15 }}
-              type='number'
-              dataKey='date'
-              domain={['dataMin', 'dataMax']}
-              tickFormatter={value => new Date(value).toLocaleDateString(locale)}
-            />
-            <YAxis />
-            <Tooltip
-              labelFormatter={value => new Date(value).toLocaleDateString(locale)}
-              formatter={value => [numberFormat.format(value), "Juostu"]}
-            />
-          </BarChart>
-        </ResponsiveContainer>
-      </div>
+      <h2>Tilastot</h2>
+      <h3>Juoksumäärä</h3>
+      <ResponsiveContainer height={350}>
+        <LineChart data={linedata}>
+          <Line dataKey='length' stroke='#FF0000' dot={{ fill: '#FF0000' }} />
+          <XAxis
+            type='number'
+            dataKey='date'
+            domain={['dataMin', 'dataMax']}
+            tickFormatter={value => new Date(value).toLocaleDateString(locale)}
+          />
+          <YAxis />
+          <Tooltip
+            labelFormatter={value => new Date(value).toLocaleDateString(locale)}
+            formatter={value => [numberFormat.format(value), "juostu"]}
+          />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 }
